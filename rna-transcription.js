b52d86f9bec3ce62e1f137dfe3adef012a0c624d
Brodie Carlisle <brodiecarlisle@gmail.com>
2019-08-06 08:36:54 -0600

    adjusted code

--- rna-transcription.js
+++ rna-transcription.js
@@ -1,19 +1,14 @@
-import { arrayExpression } from "@babel/types";
-
-//
-// This is only a SKELETON file for the 'RNA Transcription' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
 
 export const toRna = (dnaString) => {
-  const dnaToRnaMap =
+  
+    // string.replace(searchvalue, newvalue) searches string, replaces with values in dnaToRnaMap
+    // '/g' searches/replaces globaly 
+  return dnaString.replace(/[G,C,T,A]/g, m => dnaToRnaMap[m])
+};
+const dnaToRnaMap =
   {
     "G" : "C",
     "C" : "G",
     "T" : "A",
-    "A" : "U"};
-  
-    // string.replace(searchvalue, newvalue) searches string, replaces with values in dnaToRnaMap
-    // '/g' searches/replaces globaly 
-  return dnaString.replace(/["G","C","T","A"]/g, m => dnaToRnaMap[m])
-};
+    "A" : "U"
+  };
